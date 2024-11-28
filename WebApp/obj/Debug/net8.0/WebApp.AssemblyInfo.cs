becbf4d081625e1e2754391977ac0d15b4a02ef3
Silber16 <fransilber16@gmail.com>
2024-11-28 01:41:46 -0300

    view controller default

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bea38065efafab0332097fb4a3fd115092c62be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0652dee474f7319bfeed7affaab3d1ee07d741cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
